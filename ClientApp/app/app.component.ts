694d37238bc1d894bec6ce606fff287d12de3251
sameer zuhairi <sameerz09@hotmail.com>
2023-04-04 20:45:07 +0200

    First Push

---
+++ ClientApp/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = ' Sameer Angular Developer Still in Learning phase';
+   // title = no_such_object;
+
+      //  title = no_such_object;
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            ViewBag.Message = "I Like dotnet";
+            return View();
+        }
+    }
+}

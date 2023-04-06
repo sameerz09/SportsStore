0f9f4a100b3498709c5feafc481a5ba9dfcbb754
SAMEER ZUHAIRI <sameerz09@hotmail.com>
2023-04-06 21:07:34 +0300

    test

--- wwwroot/dist/inline.bundle.js
+++ wwwroot/dist/inline.bundle.js
@@ -86,7 +86,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "00d4e30798bd06d5db24"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "bade21a9e9d255e68f7b"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
